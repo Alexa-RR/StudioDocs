5f2ab804fa56a7f624f7466594e01a531316b5c9
Amit Dvir <53471801+Alexa-RR@users.noreply.github.com>
2023-02-16 16:38:55 +0200

    Add "Edit this page"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,6 +36,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/alexa-RR/StudioDocs/tree/main',
           routeBasePath: '/',
         },
         theme: {
